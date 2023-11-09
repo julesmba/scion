fdd1900dc4e54e691a0964f4185d9c3fa35d7e09
Jules Bachmann <julesb@student.ethz.ch>
2023-11-09 16:58:42 +0100

    added additional UT for flyoverhopfield testing non-flyover hops

--- pkg/slayers/path/hummingbird/flyoverhopfield_test.go
+++ pkg/slayers/path/hummingbird/flyoverhopfield_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFlyoverHopSerializeDecode(t *testing.T) {
+func TestFlyoverHopSerializeDecodeFlyover(t *testing.T) {
 	want := &hummingbird.FlyoverHopField{
 		HopField: path.HopField{
 			IngressRouterAlert: true,
@@ -23,6 +23,30 @@ func TestFlyoverHopSerializeDecode(t *testing.T) {
 		Bw:           23,
 		ResStartTime: 233,
 		Duration:     11,
+	}
+	b := make([]byte, hummingbird.FlyoverLen)
+	assert.NoError(t, want.SerializeTo(b))
+
+	got := &hummingbird.FlyoverHopField{}
+	assert.NoError(t, got.DecodeFromBytes(b))
+	assert.Equal(t, want, got)
+}
+
+func TestFlyoverHopSerializeDecode(t *testing.T) {
+	want := &hummingbird.FlyoverHopField{
+		HopField: path.HopField{
+			IngressRouterAlert: true,
+			EgressRouterAlert:  false,
+			ExpTime:            63,
+			ConsIngress:        1,
+			ConsEgress:         0,
+			Mac:                [path.MacLen]byte{1, 2, 3, 4, 5, 6},
+		},
+		Flyover:      false,
+		ResID:        0,
+		Bw:           0,
+		ResStartTime: 0,
+		Duration:     0,
 	}
 	b := make([]byte, hummingbird.FlyoverLen)
 	assert.NoError(t, want.SerializeTo(b))
